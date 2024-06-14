abfa4d4dd47e47f889a5ff7fe80514784405662d
mintaeKim <peterzzo@naver.com>
2024-06-14 15:44:09 +0900

    복습 and settime change

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));//wait 1 sec test.
+  await new Promise((resolve) => setTimeout(resolve, 3000));//wait 1 sec test.
   const response = await fetch(URL);
   const json = await response.json();
   return json;
